6cd33dc42f086bf0a44dcef80660b5047efe5be6
Alex <alexzeus@ukr.net>
2021-10-20 17:03:00 +0300

    update uppercase

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const shadow = "0 16px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19)"
 const sbg="2px"
 const bbg="4px"
 
-const aboutmes = "<h3>I repaired complex medical equipment. I had to use Raspberry \
+const aboutMes = "<h3>I repaired complex medical equipment. I had to use Raspberry \
                 on one of the devices. Read Learning Python, Programming Python\
                 5th Edition by Mark Lutz, 4 volumes. Wrote software for the device.\
                 Soon there was a need to optimize the work of the security system \
@@ -17,7 +17,7 @@ const aboutmes = "<h3>I repaired complex medical equipment. I had to use Raspber
                 is full stack samurai path, device control over sockets, web, eth. In search of interesting\
                 technology projects. From myself I can give a lot to the team and\
                 multiply my engineering skills in dev. See you…</h3>"
-const edumes =   "<h2>Beetroot Academy</h2>\
+const eduMes =   "<h2>Beetroot Academy</h2>\
                 08.02.2021 — 12.02.2021   A+<br><br>\
                 <h2>University of the state fiscal service Ukraine</h2>\
                 2010 – 2013<br>\
@@ -25,7 +25,7 @@ const edumes =   "<h2>Beetroot Academy</h2>\
                 <h2>State University of Telecommunications</h2>\
                 2004 — 2008<br>\
                 Master's degree, Protection of information in information systems and networks.   A+<br>"
-const expmes =    "<h2>StarLightMedia - Junior Python developer</h2>\
+const expMes =    "<h2>StarLightMedia - Junior Python developer</h2>\
                 01.03.2021 — to date<br>\
                 Create python database multiservices/application<br><br>\
                 <h2>StarLightMedia  -  CSO(security systems engineer)</h2>\
@@ -36,7 +36,7 @@ const expmes =    "<h2>StarLightMedia - Junior Python developer</h2>\
                 2007 — 2017<br>\
                 Full cycle of implementation of technical security systems.\
                 Service Center Management. Development of security systems<br><br>"
-const skilmes =   "<h2>Lang/Libraries/Modules/Frameworks:</h2>\
+const skilMes =   "<h2>Lang/Libraries/Modules/Frameworks:</h2>\
                 Python, JS, CSS(flaxbox, grid), Bootstrap5, HTML, Django, MySql, SQLite, mongoDB(cloud), Redis, OS, SYS, Argparse, Tkinter, PyQt, Socket, Decimal, Random, Struct, Array,\
                 Doctest, Pytest, Unittest,  PyUSB, HIDUSB, Threading,\
                 Multiprocessing, Embedd Python Ex, Keyboard, mako, Jinja2, Logging, Numpy, Matplotlib,\
@@ -44,12 +44,12 @@ const skilmes =   "<h2>Lang/Libraries/Modules/Frameworks:</h2>\
                 <h2>Additional:</h2>\
                 Win, Linux(Ubuntu), heroku,  AWS, GIT, Github, Tortoise SVN, PyCharm, Visual Studio Code, Slack, Codespaces beta, \
                 Technical background. "
-const contmes = " <h2>PHONE:</h2><br>\
+const contMes = " <h2>PHONE:</h2><br>\
                 <h3>+38-067-381-80-46  (Telegram, Viber)</h3><br>\
                 <h2>E-MAIL:</h2><br>\
                 <h3>Kaplunalex1985@gmail.com</h3><br>\
                 <h2>KYIV REGION</h2>"
-const backmes = "<h2>Electrical and Electronics Engineer:</h2><br>\
+const backMes = "<h2>Electrical and Electronics Engineer:</h2><br>\
                 Analog electronics, digital electronics, consumer electronics, embedded systems\
                 solid-state physics, radio engineering, telecommunications, control systems,\
                 signal processing, systems engineering, computer engineering, instrumentation\
@@ -58,95 +58,100 @@ const backmes = "<h2>Electrical and Electronics Engineer:</h2><br>\
                 Personal electric transport, audio amplifiers, Raspberry pi embedded programming,\
                 cnc machining assembly processing, welding(TIG, MIG, MMA), Solidworks design,\
                 EasyEDA design and manufacturing, assembly of metal structures and mechanisms."
-const lnmes = "<h2> Python, JS, ENG(intermediate), UA and others...</h2>"
-const sertmes = "<img src='img/SERT.jpg' class='img-fluid mx-auto d-block'></img>"
+const lnMes = "<h2> Python, JS, ENG(intermediate), UA and others...</h2>"
+const sertMes = "<img src='img/SERT.jpg' class='img-fluid mx-auto d-block'></img>"
 
-function borderW (point){
-    [abbut, edbut, exbut, skbut, csbut, bkbut, lnbut, cnbut].map(a => {
+function borderWidth (point){
+    [abBut, edBut, exBut, skBut, csBut, bkBut, lnBut, cnBut].map(a => {
     a.style.borderWidth = sbg
 })
 point.style.borderWidth=bbg
 }
 
-const headsection = document.getElementById('header')
-const footersection = document.getElementById('footer')
-const maininfo = document.getElementById('main_info')
-const themebut = document.getElementById('suntheme')
+const headSection = document.getElementById('header')
+const footerSection = document.getElementById('footer')
+const mainInfo = document.getElementById('main_info')
+const themeBut = document.getElementById('suntheme')
 
-let sunlebut = document.getElementById('sun')
-    sunlebut.onclick=function(){
-        if (headsection.style.backgroundColor=="black") {
-            themebut.className="icon-moon"
-            headsection.style.backgroundColor="green"
-            footersection.style.backgroundColor="green"
+let colorScheme = false
+const sunleBut = document.getElementById('sun')
+    sunleBut.onclick=function(){
+        if (!colorScheme) {
+            console.log("blk")
+            themeBut.className="icon-moon"
+            headSection.style.backgroundColor="green"
+            footerSection.style.backgroundColor="green"
+            colorScheme = !colorScheme
         } 
         else {
-            headsection.style.backgroundColor="black"
-            footersection.style.backgroundColor="black"
-            themebut.className="icon-sun"
+            console.log("noblk")
+            headSection.style.backgroundColor="black"
+            footerSection.style.backgroundColor="black"
+            themeBut.className="icon-sun"
+            colorScheme = !colorScheme
         }
     }
 
-const abbut = document.getElementById('ab_but')
-    abbut.onmouseover=function(){abbut.style.boxShadow=shadow}
-    abbut.onmouseleave=function(){abbut.style.boxShadow="none"}
-    abbut.onclick=function() {
-        maininfo.innerHTML=aboutmes
-        borderW(abbut)
+const abBut = document.getElementById('ab_but')
+    abBut.onmouseover=function(){abBut.style.boxShadow=shadow}
+    abBut.onmouseleave=function(){abBut.style.boxShadow="none"}
+    abBut.onclick=function() {
+        mainInfo.innerHTML=aboutMes
+        borderWidth(abBut)
     }
 
-const edbut = document.getElementById('ed_but')
-    edbut.onmouseover=function(){edbut.style.boxShadow=shadow}
-    edbut.onmouseleave=function(){edbut.style.boxShadow="none"}
-    edbut.onclick=function() {
-        maininfo.innerHTML=edumes
-        borderW(edbut)
+const edBut = document.getElementById('ed_but')
+    edBut.onmouseover=function(){edBut.style.boxShadow=shadow}
+    edBut.onmouseleave=function(){edBut.style.boxShadow="none"}
+    edBut.onclick=function() {
+        mainInfo.innerHTML=eduMes
+        borderWidth(edBut)
     }
 
-const exbut = document.getElementById('ex_but')
-    exbut.onmouseover=function(){exbut.style.boxShadow=shadow}
-    exbut.onmouseleave=function(){exbut.style.boxShadow="none"}
-    exbut.onclick=function() {
-        maininfo.innerHTML=expmes
-        borderW(exbut)
+const exBut = document.getElementById('ex_but')
+    exBut.onmouseover=function(){exBut.style.boxShadow=shadow}
+    exBut.onmouseleave=function(){exBut.style.boxShadow="none"}
+    exBut.onclick=function() {
+        mainInfo.innerHTML=expMes
+        borderWidth(exBut)
     }
 
-const skbut = document.getElementById('sk_but')
-    skbut.onmouseover=function(){skbut.style.boxShadow=shadow}
-    skbut.onmouseleave=function(){skbut.style.boxShadow="none"}
-    skbut.onclick=function() {
-        maininfo.innerHTML=skilmes
-        borderW(skbut)
+const skBut = document.getElementById('sk_but')
+    skBut.onmouseover=function(){skBut.style.boxShadow=shadow}
+    skBut.onmouseleave=function(){skBut.style.boxShadow="none"}
+    skBut.onclick=function() {
+        mainInfo.innerHTML=skilMes
+        borderWidth(skBut)
     }
 
-const csbut = document.getElementById('cs_but')
-    csbut.onmouseover=function(){csbut.style.boxShadow=shadow}
-    csbut.onmouseleave=function(){csbut.style.boxShadow="none"}
-    csbut.onclick=function() {
-        maininfo.innerHTML=sertmes
-        borderW(csbut)
+const csBut = document.getElementById('cs_but')
+    csBut.onmouseover=function(){csBut.style.boxShadow=shadow}
+    csBut.onmouseleave=function(){csBut.style.boxShadow="none"}
+    csBut.onclick=function() {
+        mainInfo.innerHTML=sertMes
+        borderWidth(csBut)
     }
 
-const bkbut = document.getElementById('bk_but')
-    bkbut.onmouseover=function(){bkbut.style.boxShadow=shadow}
-    bkbut.onmouseleave=function(){bkbut.style.boxShadow="none"}
-    bkbut.onclick=function() {
-        maininfo.innerHTML=backmes
-        borderW(bkbut)
+const bkBut = document.getElementById('bk_but')
+    bkBut.onmouseover=function(){bkBut.style.boxShadow=shadow}
+    bkBut.onmouseleave=function(){bkBut.style.boxShadow="none"}
+    bkBut.onclick=function() {
+        mainInfo.innerHTML=backMes
+        borderWidth(bkBut)
     }
 
-const lnbut = document.getElementById('ln_but')
-    lnbut.onmouseover=function(){lnbut.style.boxShadow=shadow}
-    lnbut.onmouseleave=function(){lnbut.style.boxShadow="none"}
-    lnbut.onclick=function() {
-        maininfo.innerHTML=lnmes
-        borderW(lnbut)
+const lnBut = document.getElementById('ln_but')
+    lnBut.onmouseover=function(){lnBut.style.boxShadow=shadow}
+    lnBut.onmouseleave=function(){lnBut.style.boxShadow="none"}
+    lnBut.onclick=function() {
+        mainInfo.innerHTML=lnMes
+        borderWidth(lnBut)
     }
 
-const cnbut = document.getElementById('cn_but')
-    cnbut.onmouseover=function(){cnbut.style.boxShadow=shadow}
-    cnbut.onmouseleave=function(){cnbut.style.boxShadow="none"}
-    cnbut.onclick=function() {
-        maininfo.innerHTML=contmes
-        borderW(cnbut)
+const cnBut = document.getElementById('cn_but')
+    cnBut.onmouseover=function(){cnBut.style.boxShadow=shadow}
+    cnBut.onmouseleave=function(){cnBut.style.boxShadow="none"}
+    cnBut.onclick=function() {
+        mainInfo.innerHTML=contMes
+        borderWidth(cnBut)
     }
